268270faf0c5335547f3cbdf8408cd29eb5080ac
Pranjal Shukla <pranjalshukla@Pranjals-MacBook-Air.local>
2024-08-21 18:34:16 +0530

    Add env

--- src/config/axios.jsx
+++ src/config/axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 const instance = axios.create(
 
     {
-        baseURL: "http://localhost:3000",
+        baseURL: import.meta.VITE_BASE_URL,
         withCredentials: true,
 
     });
